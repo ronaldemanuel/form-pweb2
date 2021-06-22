7e6fd529862ea76b4a3d4b912ccaff804a911480
Ronald <emronald15el@gmail.com>
2021-06-22 20:31:51 -0300

    estilos adicionados

--- print.php
+++ print.php
@@ -1,6 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="print-container container">
+        <?php
+            session_start();
 
-session_start();
+            echo "Nome: " . $_SESSION["name"] . "<br>";
+            echo "Telefone: " . $_SESSION["phone"];
+        ?>
+    </div>
+</body>
+</html>
 
-echo "Nome: " . $_SESSION["name"] . "<br>";
-echo "Telefone: " . $_SESSION["phone"];
--- script.php
+++ script.php
@@ -17,9 +17,12 @@ $_SESSION["phone"] = $_REQUEST["phone"];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acesso</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <a href="print.php">Link de acesso</a>
+    <div class="container link-container">
+        <a href="print.php">Link de acesso</a>
+    </div>
 </body>
 </html>
 
